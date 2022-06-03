8b55b21a12ba97cb5b0703285c46a7ac99a79cc2
Kapibaralover2115 <106837892+Kapibaralover2115@users.noreply.github.com>
2022-06-03 21:04:04 +0200

    Update Challenge 14 - Implement Quicksort.js

--- Challenge 14 - Implement Quicksort.js
+++ Challenge 14 - Implement Quicksort.js
@@ -5,6 +5,32 @@ var partition=function(array, p, r){
     // as you will implement it yourself in next challenge
     var e=array,t=p,n=r;var r=function(e,t,n){var r=e[t];e[t]=e[n];e[n]=r;};var i=t;for(var s=t;s<n;s++){if(e[s]<=e[n]){r(e,s,i);i++;}}r(e,n,i);return i;
 };
+background(124, 20, 184); // wooden table
+
+fill(219, 148, 27);
+ellipse(200, 200, 350, 350); // plate
+
+fill(230, 230, 16);
+ellipse(200, 200, 300, 310);
+
+fill(255, 0, 0);
+ellipse(271,108,55,51);
+
+fill(255,32,3);
+ellipse(302,202,60,58);
+
+ellipse(130,111,54,53);
+fill(255, 0, 0);
+
+ellipse(144,305,54,53);
+fill(255, 0, 0);
+
+ellipse(103,218,54,53);
+fill(255, 0, 0);
+
+ellipse(258,297,54,53);
+
+ellipse(199,198,54,53);
 
 
 var quickSort = function(array, p, r) {
